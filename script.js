510bd0f8d847cb7cf799d9a7fc6fd580286bcf37
DmitriyNov <NovokrDm@yandex.ru>
2022-09-28 18:24:44 +0300

    feat: edit folder icon

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 window.onload = () => {
 	let folder = document.querySelectorAll('.icon')
+	let folderOpen = false
 
 	folder.forEach(el => {
 
 		el.addEventListener('click', () =>{
-			el.parentElement.classList.toggle('active')
+			let folderElement = el.parentElement
+			folderElement.classList.toggle('active')
+			if (folderElement.getAttribute('class').includes('active')){
+				el.children[0].setAttribute('src', 'img/opened-folder.svg')
+			} else {
+				el.children[0].setAttribute('src', 'img/folder.svg')
+			}
 			el.classList.toggle('icon-opened')
 		})
 	})
